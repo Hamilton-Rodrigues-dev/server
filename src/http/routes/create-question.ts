45da9f90f4e762093c7c91408e03401c5959a371
Hamilton <hamilton.rodrigues.teixeira@gmail.com>
2025-07-23 18:34:50 -0300

    fix: sending questions to the database

--- src/http/routes/create-question.ts
+++ src/http/routes/create-question.ts
@@ -1,4 +1,3 @@
-import { and, eq, sql } from "drizzle-orm";
 import type { FastifyPluginCallbackZod } from "fastify-type-provider-zod";
 import { z } from "zod/v4";
 import { db } from "../../db/connection.ts";
@@ -26,31 +25,33 @@ export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
 
       const embeddingsAsString = `[${embeddings.join(",")}]`;
 
-      const chunks = await db
-        .select({
-          id: schema.audioChunks.id,
-          transcription: schema.audioChunks.transcription,
-          similarity: sql<number>`1 - (${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector)`,
-        })
-        .from(schema.audioChunks)
-        .where(
-          and(
-            eq(schema.audioChunks.roomId, roomId),
-            sql`1 - (${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector) > 0.7`
-          )
-        )
-        .orderBy(
-          sql`${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector`
-        )
-        .limit(3);
+      // const chunks = await db
+      //   .select({
+      //     id: schema.audioChunks.id,
+      //     transcription: schema.audioChunks.transcription,
+      //     similarity: sql<number>`1 - (${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector)`,
+      //   })
+      //   .from(schema.audioChunks)
+      //   .where(
+      //     and(
+      //       eq(schema.audioChunks.roomId, roomId),
+      //       sql`1 - (${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector) > 0.7`
+      //     )
+      //   )
+      //   .orderBy(
+      //     sql`${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector`
+      //   )
+      //   .limit(3);
 
       let answer: string | null = null;
 
-      if (chunks.length > 0) {
-        const transcriptions = chunks.map((chunk) => chunk.transcription);
+      // if (chunks.length > 0) {
+      //   const transcriptions = chunks.map((chunk) => chunk.transcription);
 
-        answer = await generateAnswer(question, transcriptions);
-      }
+      //   answer = await generateAnswer(question, roomId);
+      // }
+
+answer = await generateAnswer(question, roomId);
 
       const result = await db
         .insert(schema.questions)
